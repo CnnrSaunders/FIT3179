1c08fd00038a786a1d843ed0555666f9b9fd156c
Connor Saunders <csau0007@student.monash.edu>
2024-09-21 21:57:45 +1000

    setup complete, now i Just need to get it to work with my data.

--- Week9Homework/js/vega_lite_vis.js
+++ Week9Homework/js/vega_lite_vis.js
@@ -1,4 +1,4 @@
-var vg_1 = "suicide_rate.vg.json";
+var vg_1 = "js/suicide_rate.vg.json";
 vegaEmbed("#map_chart", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
